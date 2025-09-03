2cebcbda6f77aba89cbfee490fa493aa839e193b
estebanmorocho08 <estebanmorocho08@gmail.com>
2025-09-03 21:15:59 +0000

    ✨ Feat (WelcomeText.tsx): Añadido el WelcomeText

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { WelcomeText } from "../components/WelcomeText"
 
 export default function Index() {
   return (
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome to index.tsx</Text>
+      <WelcomeText />
     </View>
   );
 }
---
+++ components/WelcomeText.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+
+export function WelcomeText() {
+    return (
+        <View>
+            <Text>Hola Mundo</Text>
+            <Text>PEPE</Text>
+        </View>
+    );
+}
