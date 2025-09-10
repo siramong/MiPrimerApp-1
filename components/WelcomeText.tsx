52cb4e1765beff0f1562c3b612e130008635b615
estebanmorocho08 <estebanmorocho08@gmail.com>
2025-09-10 19:05:50 +0000

    Revert "🐛 Fix (WelcomeText.tsx): Añadido import de Button"
    This reverts commit e81c78ef2b682acb3f9abda5d9c1e4245ca3b3f4.

--- components/WelcomeText.tsx
+++ components/WelcomeText.tsx
@@ -1,6 +1,5 @@
-import { setBackgroundColorAsync } from 'expo-system-ui';
 import React, { useState } from 'react';
-import { Pressable, Text, View, Button } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 const WelcomeText = () => {
     const [cont, setCont] = useState(0);
@@ -11,6 +10,7 @@ const WelcomeText = () => {
 
     return (
         <View>
+            <Text>WelcomeText</Text>
             <Pressable onPress={() => setCont(incrementar(cont))}>
                 <Text>Aumentar: {cont}</Text>
             </Pressable>
